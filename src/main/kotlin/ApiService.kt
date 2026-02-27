5645ad889ef785886784b6f39cacb79bbb2812fa
Igor0006 <igordubrovskij69@gmail.com>
2026-02-27 14:52:33 +0300

    feat: add limit for serialization

--- src/main/kotlin/ApiService.kt
+++ src/main/kotlin/ApiService.kt
@@ -1,28 +1,50 @@
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.DecodeSequenceMode
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import kotlinx.serialization.json.decodeToSequence
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.io.Closeable
 
-class ApiService {
+class ApiService : Closeable {
     companion object {
         private const val TASK_URL = "https://kompege.ru/api/v1/task/number/"
-        private val client = OkHttpClient()
         private val json = Json {
             ignoreUnknownKeys = true
         }
     }
+    private val client = OkHttpClient()
 
-    fun get(number: Int): List<TaskItem> {
+    @OptIn(ExperimentalSerializationApi::class)
+    fun get(number: Int, limit: Int? = null): List<TaskItem> {
         val req = Request.Builder()
             .url(TASK_URL + number.toString())
             .header("Accept", "application/json")
             .header("User-Agent", "Mozilla/5.0")
             .build()
-        val response = client.newCall(req).execute().use { call ->
+        return client.newCall(req).execute().use { call ->
             if (!call.isSuccessful) {
                 error("HTTP ${call.code}: ${call.message}")
             }
-            call.body?.string() ?: error("Empty response")
+            val body = call.body ?: error("Empty response")
+            if (limit == null) {
+                json.decodeFromStream(body.byteStream())
+            } else {
+                require(limit >= 0) { "limit must be >= 0" }
+                json.decodeToSequence<TaskItem>(
+                    stream = body.byteStream(),
+                    format = DecodeSequenceMode.ARRAY_WRAPPED
+                ).take(limit).toList()
+            }
         }
-        return json.decodeFromString(response)
+    }
+
+    override fun close() {
+        client.dispatcher.cancelAll()
+        client.dispatcher.executorService.shutdown()
+        client.dispatcher.executorService.shutdownNow()
+        client.connectionPool.evictAll()
+        client.cache?.close()
     }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,8 @@
 fun main() : Unit {
-    val apiService = ApiService()
-    val tasks = apiService.get(1)
-    tasks.forEach { task ->
-        println("taskId=${task.taskId}, number=${task.number}, key=${task.key}")
+    ApiService().use { apiService ->
+        val tasks = apiService.get(1, 2)
+        tasks.forEach { task ->
+            println("taskId=${task.taskId}, number=${task.number}, key=${task.key}")
+        }
     }
 }
